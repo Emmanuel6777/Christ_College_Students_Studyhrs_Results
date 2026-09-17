8ed9bc6f06c07750a0fb4c9ce9f17a55dc0334f3
Emmanuel6777 <149251796+Emmanuel6777@users.noreply.github.com>
2026-09-17 11:42:21 +0530

    Update model loading and prediction input structure

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import joblib
 
-model = joblib.load("logistic_regression_Student__Studyhrs_Results_model.pkl")
+model = joblib.load("student_studyhrs_withatt_model.pkl")
 
 st.title("Student Pass/Fail Based on study Hours")
 
 hours=st.number_input("Enter study hours",min_value=0.0,max_value=15.0,value=5.0)
+attendance=st.number_input("Enter attendance hours",min_value=0.0,max_value=100.0,value=75.0)
 
+input_data=pd.DataFrame({
+  "StudyHours":[hours],
+  "attendance":[attendance]
+})
 if st.button("Predict"):
-  prediction = model.predict([[hours]])
-  prob=model.predict_proba([[hours]])
+  prediction = model.predict(input_data)
+  prob=model.predict_proba(input_data)
   predictedpass=prob[0][1]
   if prediction[0]==1:
     st.success("PASS")
